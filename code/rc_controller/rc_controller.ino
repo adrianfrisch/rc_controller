b3235e6401a4c38e26eec74962b7e60f355a45ac
Adrian Frischauf <adrian.frischauf@gmail.com>
2021-11-22 16:58:12 +0100

    Mode selection

--- code/rc_controller/rc_controller.ino
+++ code/rc_controller/rc_controller.ino
@@ -1,4 +1,6 @@
 #include <RingBuf.h>
+#include <SPI.h> 
+#include <nRF24L01.h>
 #include <RF24.h>
 #include <LiquidCrystal.h>
 // Keep in Mind: Bluetooth modules need pairing before they send / receive data.
@@ -16,16 +18,15 @@
 #define AXIS2_Z_PIN A6
 
 // Using interrupt capable digital pins for the 4 pushbuttons
-#define BUTTON_1 18
-#define BUTTON_2 19
-#define BUTTON_3 20
+#define BUTTON_1 19
+#define BUTTON_2 20
+#define BUTTON_3 18
 #define BUTTON_4 21
 
 // Connectors for the NRF24L01
 #define NRF_MISO 50 
 #define NRF_MOSI 51
-#define NRF_SS 52 
-#define NRF_SCK 53
+#define NRF_SCK 52 
 #define NRF_CE 2
 #define NRF_CSN 3
 
@@ -73,10 +74,41 @@ LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_D0, LCD_D1, LCD_D2, LCD_D3, LCD_D4, LC
 RF24 radio(NRF_CE,NRF_CSN);
 const byte address[6] = "00001";
 
-void setup(){
-    Serial.begin(9600);
-    // Bluetooth Serial on Serial2
-    Serial2.begin(9600);
+#define MODE_SETUP 0
+#define MODE_BT 1
+#define MODE_24GHZ 2
+
+int mode = MODE_SETUP;
+int cursorPosition = 0;
+int removed;
+
+void modeSelection() {
+    lcd.setCursor(2,0);
+    lcd.print("Bluetooth Mode");
+    lcd.setCursor(2,1);
+    lcd.print("Wifi Mode");
+
+    lcd.setCursor(0,0);
+    lcd.print(">");
+
+    while (mode == MODE_SETUP){
+      lcd.setCursor(0,0);
+      lcd.print(" ");
+      lcd.setCursor(0,1);
+      lcd.print(" ");
+      lcd.setCursor(0,cursorPosition);
+      lcd.print(">");
+      delay(50);
+    }
+    lcd.clear();
+    lcd.setCursor(14,0);
+    if (mode == MODE_BT){
+      lcd.print("BT");
+    } else {
+      lcd.print("24");
+    }
+}
+void configurePushbuttons() {
     pinMode(BUTTON_1,INPUT_PULLUP);
     pinMode(BUTTON_2,INPUT_PULLUP);
     pinMode(BUTTON_3,INPUT_PULLUP);
@@ -85,6 +117,13 @@ void setup(){
     attachInterrupt(digitalPinToInterrupt(BUTTON_2),button2Pressed,FALLING);
     attachInterrupt(digitalPinToInterrupt(BUTTON_3),button3Pressed,FALLING);
     attachInterrupt(digitalPinToInterrupt(BUTTON_4),button4Pressed,FALLING);
+}
+
+void setup(){
+    Serial.begin(9600);
+    // Bluetooth Serial on Serial2
+    Serial2.begin(9600);
+    configurePushbuttons();
     // Initially fill the ringbuffers
     for (int i=0 ; i<BUFFER_SIZE ;i++){
         x1.push(0); 
@@ -94,12 +133,14 @@ void setup(){
         y2.push(0);
         z2.push(0);
     }
-    
-    radio.begin();
-    radio.openWritingPipe(address);
-    radio.setPALevel(RF24_PA_MAX);
     lcd.begin(16, 2);
     lcd.clear();
+    modeSelection();
+    Serial.println(radio.begin());
+    radio.openWritingPipe(address);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.stopListening();
+    
 }   
 
 int average(RingBuf<int,BUFFER_SIZE> *buff){
@@ -110,18 +151,16 @@ int average(RingBuf<int,BUFFER_SIZE> *buff){
     }
     return sum/BUFFER_SIZE;
 }
-
-int removed;
-void loop(){
-    lcd.setCursor(0,0);
-    lcd.print("Universal Remote");
-    lcd.setCursor(0, 1);
-    // Print a message to the LCD.
-    lcd.print("LCD Check");
-    
+void readAllAnalogPositions() {
     axis1_x = analogRead(AXIS1_X_PIN);
+    lcd.setCursor(0, 1);
+    lcd.print(axis1_x);
     axis1_y = analogRead(AXIS1_Y_PIN);
+    lcd.setCursor(5,1);
+    lcd.print(axis1_y);
     axis1_z = analogRead(AXIS1_Z_PIN);
+    lcd.setCursor(10,1);
+    //lcd.print(axis1_z);
     x1.pop(removed);
     y1.pop(removed);
     z1.pop(removed);
@@ -138,29 +177,88 @@ void loop(){
     x2.push(axis2_x);
     y2.push(axis2_y);
     z2.push(axis2_z);
-    
+}
+
+void printStatusToSerial() {
     Serial.print(average(&x1));Serial.print(',');
     Serial.print(average(&y1));Serial.print(',');
-    Serial.print(average(&z1));Serial.print(',');
+    //Serial.print(average(&z1));Serial.print(',');
     Serial.print(average(&x2));Serial.print(',');
     Serial.print(average(&y2));Serial.print(',');
-    Serial.print(average(&z2));Serial.print(',');
+    //Serial.print(average(&z2));Serial.print(',');
     Serial.print(b1);Serial.print(',');
     Serial.print(b2);Serial.print(',');
     Serial.print(b3);Serial.print(',');
-    Serial.print(b4);Serial.print('\n');
+    Serial.print(b4);Serial.print(',');
+    Serial.print(analogRead(BATTERY_STATUS_PIN));Serial.print('\n');
+}
+
+void loop(){
+    lcd.setCursor(0,0);
+    lcd.print("RC-Remote");
     
+    readAllAnalogPositions();            
+    switch (mode){
+    case MODE_24GHZ:
+      const char text[] = "Hello World";
+      //if (!radio.write(&text, sizeof(text))){
+      //  Serial.println("Send failed");             
+      //}
+      delay(250); 
+      break;
+    case MODE_BT:
+      char command = getCommand(axis1_y,axis2_x);
+      Serial.print(command);
+      Serial2.print(command);
+      delay(50);
+      break;    
+    default:
+      delay(250); 
+      break;
+    }
+    printStatusToSerial();
+       
 }
 
 void button1Pressed(){
+  Serial.println("B1");
+  switch (mode) {
+  case MODE_SETUP:
+    cursorPosition = cursorPosition == 0 ? 1 : 0;
+    break;
+  default:
     b1 = (b1 == 0 ? 1 : 0);
+    break;
+  }
 }
 void button2Pressed(){
+  Serial.println("B2");
+  switch (mode){
+  case MODE_SETUP:
+    cursorPosition = cursorPosition == 0 ? 1 : 0;
+    break;
+  default:
     b2 = (b2 == 0 ? 1 : 0);
+    break;
+  }
 }
 void button3Pressed(){
+    Serial.println("B3");
     b3 = (b3 == 0 ? 1 : 0);
 }
 void button4Pressed(){
+  Serial.println("B4");
+  Serial.println(mode);
+  switch (mode) {
+  case MODE_SETUP:
+    if (cursorPosition == 0) {
+      mode = MODE_BT;
+    } else {
+      mode = MODE_24GHZ;
+    }
+    break;
+  default:
     b4 = (b4 == 0 ? 1 : 0);
+    break;
+  }
 }
--- code/rc_receiver_dummy/rc_receiver_dummy.ino
+++ code/rc_receiver_dummy/rc_receiver_dummy.ino
@@ -1,25 +1,27 @@
 // A simple receiver sketch
 // 
-
+#include <SPI.h> 
+#include <nRF24L01.h>
 #include <RF24.h>
 
 #define NRF_CE 7
 #define NRF_CSN 8
 
-RF24 radio(NRF_CE,NRF_CSN);
+RF24 radio(NRF_CSN,NRF_CE);
 const byte address[6] = "00001";
 
 void setup() {
   Serial.begin(9600);
-  radio.begin();
+  Serial.print(radio.begin());
   radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MAX);
+  radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 }
-void loop() {
+void loop() { 
   if (radio.available()) {
     char text[32] = "";
     radio.read(&text, sizeof(text));
-    Serial.println(text);
-  }
+    Serial.print("Rec: ");
+    Serial.println(text); 
+  }  
 }
